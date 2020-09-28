4b7d60297ff2ebe499b327d266959af4ab026102
Paul Lakra <60219734+Marvin101@users.noreply.github.com>
2020-09-28 22:26:15 +0530

    28th_fourth_liknd

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstPage } from '../first/first.page';
+import { FourthPage } from '../fourth/fourth.page';
+import { SecondPage } from '../second/second.page';
+import { ThirdPage } from '../third/third.page';
 
 import { HomePage } from './home.page';
 
@@ -7,6 +11,22 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage
+  },
+  {
+    path: 'first',
+    component: FirstPage
+  },
+  {
+    path: 'second',
+    component: SecondPage
+  },
+  {
+    path: 'third',
+    component: ThirdPage
+  },
+  {
+    path: 'fourth',
+    component: FourthPage
   }
 ];
 
@@ -14,4 +34,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class HomePageRoutingModule {}
+export class HomePageRoutingModule { }
